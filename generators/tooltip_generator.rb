29fc85a1ddb97e0f9644127f281285bd84a0a3e2
Peter Jackson <pete@peteonrails.com>
2009-09-24 00:36:14 -0400

    Move stuff, make stuff work.

--- generators/tooltip_generator.rb
+++ generators/tooltip_generator.rb
@@ -3,7 +3,7 @@ class TooltipGenerator < Rails::Generator::Base
   def manifest  
     record do |m|
       m.file "tooltip.css", "public/stylesheets/tooltip.css"
-      m.file "tooltip.sass", "public/stylesheets/tooltip.sass"
+      m.file "tooltip.sass", "public/stylesheets/sass/tooltip.sass"
       m.insert_into "app/controllers/application_controller.rb", "helper :tooltip"
       m.insert_into "app/layouts/application.html.haml", "= yield :javascript"
       m.readme "INSTRUCTIONS"
